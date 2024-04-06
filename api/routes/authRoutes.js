a654449ccedc52fc23a35c718064ced5b90c3cf1
Fafli4ko <131291736+Fafli4ko@users.noreply.github.com>
2024-04-06 15:35:32 +0300

    switchedd form bcrypt to bcryptjs

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,7 +1,7 @@
 // authRoutes.js
 
 const express = require("express");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const mongoose = require("mongoose");
