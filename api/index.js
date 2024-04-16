548e8c84198f41cc6055f83738a1a2d75669c976
Fafli4ko <131291736+Fafli4ko@users.noreply.github.com>
2024-04-16 18:16:45 +0300

    small fixes and comment tidied up in index.js

--- api/index.js
+++ api/index.js
@@ -1,25 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const User = require("./models/User");
-const Books = require("./models/Books");
-const Movies = require("./models/Movies");
-const Shows = require("./models/Shows");
 const cookieParser = require("cookie-parser");
-const imageDownloader = require("image-downloader");
-const multer = require("multer");
-const fs = require("fs");
+
 require("dotenv").config();
 const app = express();
 
-const bcryptSalt = bcrypt.genSaltSync(5);
-const jwtSecret = "thisIsARandomStringForTheJWTToken";
-
 app.use(express.json());
 app.use(cookieParser());
-app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
@@ -27,15 +14,8 @@ app.use(
   })
 );
 
-//mongoose.connect(process.env.MONGO_URL);
-//media-admin-parola-24
-
 //#region AuthUser stuff
 app.use("/api/auth", require("./routes/authRoutes"));
-//#endregion
-
-//#region Photo Upload stuff
-app.use("/api/photoUploader", require("./routes/photoUploaderRoutes"));
 //#endregion
 
 //#region Movie stuff
